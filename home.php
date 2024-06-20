dfd1d48c6bfe088a97b8f1b4efda583248bd42c2
dfebrywulangsih <163366070+dfebrywulangsih@users.noreply.github.com>
2024-06-20 18:09:46 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,235 @@
+  <section id="banner">
+   
+  <!-- Slider -->
+        <div id="main-slider" class="flexslider">
+            <ul class="slides">
+              <li>
+                <img src="<?php echo web_root; ?>plugins/home-plugins/img/slides/jobin1.jpg" alt="" />
+                <div class="flex-caption">
+                    <h3>innovation</h3> 
+          <p>We create the opportunities</p> 
+           
+                </div>
+              </li>
+              <li>
+                <img src="<?php echo web_root; ?>plugins/home-plugins/img/slides/jobin2.jpg" alt="" />
+                <div class="flex-caption">
+                    <h3>Specialize</h3> 
+          <p>Success depends on work</p> 
+           
+                </div>
+              </li>
+            </ul>
+        </div>
+  <!-- end slider -->
+ 
+  </section> 
+  <section id="call-to-action-2">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-10 col-sm-9">
+          <h3>Partner with Business Leaders</h3>
+          <p>Development of successful, long term, strategic relationships between customers and suppliers, based on achieving best practice and sustainable competitive advantage. In the business partner model, HR professionals work closely with business leaders and line managers to achieve shared organisational objectives.</p>
+        </div>
+       <!--  <div class="col-md-2 col-sm-3">
+          <a href="#" class="btn btn-primary">Read More</a>
+        </div> -->
+      </div>
+    </div>
+  </section>
+  
+  <section id="content">
+  
+  
+  <div class="container">
+        <div class="row">
+      <div class="col-md-12">
+        <div class="aligncenter"><h2 class="aligncenter">Company</h2><!-- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt eius magni provident, doloribus omnis minus ovident, doloribus omnis minus temporibus perferendis nesciunt.. --></div>
+        <br/>
+      </div>
+    </div>
+
+    <?php 
+      $sql = "SELECT * FROM `tblcompany`";
+      $mydb->setQuery($sql);
+      $comp = $mydb->loadResultList();
+
+
+      foreach ($comp as $company ) {
+        # code...
+    
+    ?>
+            <div class="col-sm-4 info-blocks">
+                <i class="icon-info-blocks fa fa-building-o"></i>
+                <div class="info-blocks-in">
+                    <h3><?php echo $company->COMPANYNAME;?></h3>
+                    <!-- <p><?php echo $company->COMPANYMISSION;?></p> -->
+                    <p>Address: <?php echo $company->COMPANYADDRESS;?></p>
+                    <p>Contact No: <?php echo $company->COMPANYCONTACTNO;?></p>
+                </div>
+            </div>
+
+    <?php } ?> 
+  </div>
+  </section>
+  
+  <section class="section-padding gray-bg">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="section-title text-center">
+            <h2 >Popular Jobs</h2>  
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 ">
+          <?php 
+            $sql = "SELECT * FROM `tblcategory`";
+            $mydb->setQuery($sql);
+            $cur = $mydb->loadResultList();
+
+            foreach ($cur as $result) {
+              echo '<div class="col-md-3" style="font-size:15px;padding:5px">* <a href="'.web_root.'index.php?q=category&search='.$result->CATEGORY.'">'.$result->CATEGORY.'</a></div>';
+            }
+
+          ?>
+        </div>
+      </div>
+ 
+    </div>
+  </section>    
+  <section id="content-3-10" class="content-block data-section nopad content-3-10">
+  <div class="image-container col-sm-6 col-xs-12 pull-left">
+    <div class="background-image-holder">
+
+    </div>
+  </div>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-6 col-sm-offset-6 col-xs-12 content">
+        <div class="editContent">
+          <h3>Our Team</h3>
+        </div>
+        <div class="editContent"  style="height:235px;">
+          <p> 
+          &nbsp;&nbsp;Our "one team" attitude breaks down silos and helps us engage equally effectively from the C-suite to the front line. Our collaborative working style emphasizes teamwork, trust, and tolerance for diverging opinions. People tell us we are down-to-earth, approachable and fun.<br/><br/>
+
+          &nbsp;&nbsp;We have a passion for our clients' true results and a pragmatic drive for action that starts Monday morning 8am and doesn't let up. We rally clients with our infectious energy, to make change stick.<br/><br/>
+
+          &nbsp;&nbsp;And we never go it alone. We support and are supported to develop our own personal results stories. We balance challenging and co-creating with our clients, building the internal capabilities required for them to create repeatable results. </p>
+        </div> 
+      </div>
+    </div><!-- /.row-->
+  </div><!-- /.container -->
+</section>
+  
+  <div class="about home-about">
+        <div class="container">
+            
+            <div class="row">
+              <div class="col-md-4">
+                <!-- Heading and para -->
+                <div class="block-heading-two">
+                  <h3><span>Programes</span></h3>
+                </div>
+                <p>JOBin thoroughly verifies all companies posting lockers by combining technology and a dedicated team, thus enhancing the security of the KitaLulus platform for talents. JOBin provides direct access to employers by processing over 3.5 million job applications every month. JOBin's job postings are spread across more than 30 cities, promoting equal employment opportunities from Sabang to Merauke.</p>
+              </div>
+              <div class="col-md-4">
+                <div class="block-heading-two">
+                  <h3><span>Latest News</span></h3>
+                </div>    
+                <!-- Accordion starts -->
+                <div class="panel-group" id="accordion-alt3">
+                 <!-- Panel. Use "panel-XXX" class for different colors. Replace "XXX" with color. -->
+                  <div class="panel"> 
+                  <!-- Panel heading -->
+                   <div class="panel-heading">
+                    <h4 class="panel-title">
+                      <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseOne-alt3">
+                      <i class="fa fa-angle-right"></i> 01. How can I increase my chances of getting a job?
+                      </a>
+                    </h4>
+                   </div>
+                   <div id="collapseOne-alt3" class="panel-collapse collapse">
+                    <!-- Panel body -->
+                    <div class="panel-body">
+                    Make your CV stand out from other candidates so that employers know you are the candidate they are looking for. Add relevant work experience, education and helpful skills to your CV. You can also read more job search tips on the JOBin blog.
+                    </div>
+                   </div>
+                  </div>
+                  <div class="panel">
+                   <div class="panel-heading">
+                    <h4 class="panel-title">
+                      <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseTwo-alt3">
+                      <i class="fa fa-angle-right"></i> 02. Is there a limit to the number of applications that can be submitted through JOBin?
+                      </a>
+                    </h4>
+                   </div>
+                   <div id="collapseTwo-alt3" class="panel-collapse collapse">
+                    <div class="panel-body">
+                    With JOBin, there's no limit to your ambition. We encourage you to apply for as many job opportunities as you want, while adhering to a daily limit of xx job applications. This aims to avoid spamming employers, while ensuring job seekers apply to the xx most relevant jobs every day.
+                    </div>
+                   </div>
+                  </div>
+                  <div class="panel">
+                   <div class="panel-heading">
+                    <h4 class="panel-title">
+                      <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseThree-alt3">
+                      <i class="fa fa-angle-right"></i> 03. I have a problem when applying for a job, where should I report to?
+                      </a>
+                    </h4>
+                   </div>
+                   <div id="collapseThree-alt3" class="panel-collapse collapse">
+                    <div class="panel-body">
+                    If you have any problems while applying for a job on JOBin, you can directly report them to JOBin's customer service via WhatsApp at 082140777555.
+                    </div>
+                   </div>
+                  </div>
+                  <div class="panel">
+                   <div class="panel-heading">
+                    <h4 class="panel-title">
+                      <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseFour-alt3">
+                      <i class="fa fa-angle-right"></i> 04. Are job vacancies at JOBin available in various cities in Indonesia?
+                      </a>
+                    </h4>
+                   </div>
+                   <div id="collapseFour-alt3" class="panel-collapse collapse">
+                    <div class="panel-body">
+                    Yes, JOBin is currently available in 300+ cities across Indonesia and counting.
+                    </div>
+                   </div>
+                  </div>
+                </div>
+                <!-- Accordion ends -->
+                
+              </div>
+              
+              <div class="col-md-4">
+                <div class="block-heading-two">
+                  <h3><span>Testimonials</span></h3>
+                </div>  
+                     <div class="testimonials">
+                    <div class="active item">
+                      <blockquote><p>Whenever I have a recruitment problem, the JOBin team always quickly responds and helps find a solution. The job promotion feature also really helps us get candidates quickly. The quality of incoming candidates is also in accordance with the needs of the company so that many are processed. Even those who have so far joined through JOBin many still survive.</p></blockquote>
+                      <div class="carousel-info">
+                      <img alt="" src="img/team4.jpg" class="pull-left">
+                      <div class="pull-left">
+                        <span class="testimonials-name">Brigita Fani</span>
+                        <span class="testimonials-post">PT Primafood Internasional</span>
+                      </div>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+              
+            </div>
+            
+                        
+             
+            <br>
+           
+            </div>
+            
+          </div>
